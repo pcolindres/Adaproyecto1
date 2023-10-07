644720045f8a06a6a620267302328cf5c7bebe4e
pcolindres <146043682+pcolindres@users.noreply.github.com>
2023-10-06 20:34:50 -0600

    Add files via upload
    Nueva versión proyecto Integrador segunda parte.

--- Proy1.py
+++ Proy1.py
@@ -1,3 +1,12 @@
 print("JUEGO LABERTINTO DE TEXTO HECHO POR PAOLA COLINDRES") 
 name = input ("Ingresa tu nombre:")
 print ("Bienvenid@"+ name)
+
+# Proyecto integrador 2da parte
+while True:
+    tecla= readkey()
+    if tecla= kep.UP:
+    print ("Termina el juego presionando UP")
+    break
+    print ("presiona la tecla")
+    
